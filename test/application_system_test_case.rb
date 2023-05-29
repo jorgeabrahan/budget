dc38fec7a4bd242fe6976780bd3f6708aa7580b3
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-29 15:37:45 -0600

    Setup and configure capybara

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,21 @@
-require "test_helper"
+require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium_remote_chrome
+
+  Capybara.register_driver :selenium_remote_chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--start-maximized')
+
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      options:
+    )
+  end
+
+  Capybara.configure do |config|
+    config.server_host = 'localhost'
+    config.server_port = '3000'
+  end
 end
