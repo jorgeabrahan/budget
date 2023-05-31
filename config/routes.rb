c402b6e0d75081739d45f733dc5bc6c522ad5b36
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-31 14:51:00 -0600

    Remove unnecessary routes, and modify dealing routes to belong to groups model and call the deallings controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
   root "groups#index" # categories as home page
   get '/categories', to: 'groups#index', as: 'categories'
   get '/categories/new', to: 'groups#new', as: 'new_category'
-  get '/categories/:id', to: 'groups#show', as: 'category'
 
-  get '/categories/:category_id/transactions', to: 'dealings#index', as: 'category_transactions'
-  get '/categories/:category_id/transactions/new', to: 'dealings#new', as: 'new_category_transaction'
-  get '/categories/:category_id/transactions/:id', to: 'dealings#show', as: 'category_transaction'
+  get '/categories/:category_id/transactions', to: 'groups/dealings#index', as: 'category_transactions'
+  get '/categories/:category_id/transactions/new', to: 'groups/dealings#new', as: 'new_category_transaction'
 
   resources :groups, only: [:create, :destroy] do
-    resources :dealings, only: [:create, :destroy]
+    resources :dealings, only: [:create, :destroy], controller: 'groups/dealings'
   end
 
   get '/home', to: 'home#index', as: 'home'
