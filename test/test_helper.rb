fc58b8f1fa2b88216847fe83ac28b67f940da82e
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-01 09:58:50 -0600

    Include warden in test helper to help with the sign in process

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  include Warden::Test::Helpers
+  Warden.test_mode!
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors, with: :threads)
 
